4fec5fa3d320931ee68a5c83e9e804986e9e88c3
LS4Tonio <toniolesniper@gmail.com>
2017-07-06 11:53:24 +0200

    Added content and links to home page
    Added some images
    Updated language selector position on footer

--- Portfolio/ClientApp/app/components/home/home.component.ts
+++ Portfolio/ClientApp/app/components/home/home.component.ts
@@ -10,4 +10,8 @@ export class HomeComponent {
     @Language() public lang: string;
 
     public imageSrc: any = require("./../../../assets/images/me-colored.jpg");
+    public aboutImgSrc: any = require("./../../../assets/images/me-whited.jpg");
+    public contactImgSrc: any = require("./../../../assets/images/home/contact.jpg");
+    public projectImgSrc: any = require("./../../../assets/images/home/project.jpg");
+    public skillImgSrc: any = require("./../../../assets/images/home/skill.jpg");
 }
