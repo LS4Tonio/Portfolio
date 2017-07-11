299535f4a6b8331c3451fae8e97275d1f290cd3e
Antoine BIBES <abibes@alter-frame.com>
2017-07-11 17:52:38 +0200

    Added images for skills

--- Portfolio/ClientApp/app/components/skills/skills.component.ts
+++ Portfolio/ClientApp/app/components/skills/skills.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { Language } from "angular-l10n";
 
+import { SkillModel } from "../../models/skill.model";
+
 @Component({
     selector: "skills",
     templateUrl: "./skills.component.html",
@@ -9,6 +11,26 @@ import { Language } from "angular-l10n";
 export class SkillsComponent {
     @Language() public lang: string;
 
+    public technicalSkills: SkillModel[];
+
     public imgHumanSkill: any = require("./../../../assets/images/skills/human.png");
     public imgTechSkill: any = require("./../../../assets/images/skills/tech.png");
+
+    constructor() {
+        this.technicalSkills = [
+            new SkillModel("CSharp"),
+            new SkillModel("ASP DotNet"),
+            new SkillModel("HTML CSS"),
+            new SkillModel("Javascript"),
+            new SkillModel("SCSS LESS"),
+            new SkillModel("Angular")
+        ];
+
+        //var humanSkills = [
+        //    new SkillModel("Serious"),
+        //    new SkillModel("Adaptation"),
+        //    new SkillModel("Autonomy"),
+        //    new SkillModel("Team Work")
+        //];
+    }
 }
---
+++ Portfolio/ClientApp/app/models/skill.model.ts
@@ -0,0 +1,15 @@
+﻿export class SkillModel {
+    public name: string;
+    public url: string;
+    public imageUrl: string;
+    public description: string;
+
+    constructor(name: string) {
+        const formatedName = name.trim().toLowerCase().replace(new RegExp("\s"), "-");
+
+        this.name = name;
+        this.url = formatedName;
+        this.imageUrl = require(`./../../assets/images/skills/${formatedName}.png`);
+        this.description = formatedName;
+    }
+}
