28398a5836cb38f25fc989f4bf72ada9f72a6ec2
Antoine BIBES <abibes@alter-frame.com>
2017-07-31 11:24:50 +0200

    Added localization text for contact form submit result
    Added recaptcha component

--- Portfolio/ClientApp/app/components/contact/contact.component.ts
+++ Portfolio/ClientApp/app/components/contact/contact.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { Language } from "angular-l10n";
+import { ReCaptchaComponent } from 'angular2-recaptcha';
 
 import { ContactService } from "../../services/contact.service";
 import { ContactModel } from "../../models/contact.model";
@@ -10,12 +11,14 @@ import { ContactModel } from "../../models/contact.model";
     templateUrl: "./contact.component.html"
 })
 export class ContactComponent {
+    @ViewChild(ReCaptchaComponent) private _captcha: ReCaptchaComponent;
     @Language() public lang: string;
 
     public contactModel: ContactModel;
     public emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    public messageResult: boolean;
     public emailSent: boolean = false;
+    public emailResult: number = 0;
+    public captchaValid: boolean = false;
 
     /**
      * Initializes the contact page
@@ -23,6 +26,21 @@ export class ContactComponent {
      */
     constructor(private _contactService: ContactService) {
         this.contactModel = new ContactModel();
+    }
+
+    /**
+     * Resets the captcha when expire
+     */
+    public handleCaptchaExpired(): void {
+        this._captcha.reset();
+    }
+
+    /**
+     * Allows user to valid it's form when captcha is correct
+     * @param token response token
+     */
+    public handleCorrectCaptcha(token) {
+        this.captchaValid = !!token;
     }
 
     /**
@@ -34,14 +52,17 @@ export class ContactComponent {
         this._contactService
             .postMail(this.contactModel)
             .subscribe(this.handleResponse, this.handleError);
+
+        this._captcha.reset();
     }
 
     /**
      * Handle the request response
      * @param value response
      */
-    private handleResponse(value): void {
-        this.messageResult = value.status === 200;
+    private handleResponse(value: any): void {
+        this.emailResult = value.status;
+        console.log("success", this.emailResult);
     }
 
     /**
@@ -49,6 +70,7 @@ export class ContactComponent {
      * @param error reponse error
      */
     private handleError(error: any): void {
-        this.messageResult = false;
+        this.emailResult = error.status;
+        console.log("error", this.emailResult);
     }
 }
