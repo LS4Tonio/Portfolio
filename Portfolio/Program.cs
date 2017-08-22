c8942ac1c2bc8ea488388f7fb66b3cff5319685d
LS4Tonio <toniolesniper@gmail.com>
2017-08-22 22:55:27 +0200

    Added config file for urls & port

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using System.IO;
 
 namespace Portfolio
@@ -7,11 +8,19 @@ namespace Portfolio
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+            var urls = config.GetSection("urls").Value;
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseUrls(urls)
                 .Build();
 
             host.Run();
